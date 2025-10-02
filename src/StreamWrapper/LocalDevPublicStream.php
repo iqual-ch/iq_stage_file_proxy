e203a80c0a6d06fd8048eeed62f407b9390238ec
Guillaume Pacilly <guillaume.pacilly@gmail.com>
2025-10-02 15:46:30 +0000

    fix php code sniffer notices

--- src/StreamWrapper/LocalDevPublicStream.php
+++ src/StreamWrapper/LocalDevPublicStream.php
@@ -3,10 +3,7 @@
 namespace Drupal\iq_stage_file_proxy\StreamWrapper;
 
 use Drupal\Component\Utility\UrlHelper;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\StreamWrapper\PublicStream;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Overrides the default Drupal public stream wrapper class for read operations.
@@ -15,8 +12,6 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
  * usually the production instance.
  */
 class LocalDevPublicStream extends PublicStream {
-
-  use StringTranslationTrait;
 
   /**
    * The host used to load public assets from.
@@ -33,33 +28,22 @@ class LocalDevPublicStream extends PublicStream {
   protected $offload = FALSE;
 
   /**
-   * The Config Factory service.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  protected ConfigFactoryInterface $configFactory;
-
-  /**
    * The file system service.
    *
    * @var \Drupal\Core\File\FileSystemInterface
    */
-  protected FileSystemInterface $fileSystem;
+  protected $fileSystem;
 
   /**
    * Creates a LocalDevPublicStream.
-   *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
-   *   The Config Factory service.
-   * @param \Drupal\Core\File\FileSystemInterface $fileSystem
-   *   The file system service.
    */
-  public function __construct(ConfigFactoryInterface $configFactory, FileSystemInterface $fileSystem) {
-    $this->configFactory = $configFactory;
-    $this->fileSystem = $fileSystem;
-
-    $this->remoteInstance = $this->configFactory->get('iq_stage_file_proxy.settings')->get('remote_instance');
-    $this->offload = $this->configFactory->get('iq_stage_file_proxy.settings')->get('offload') ?: $this->offload;
+  public function __construct() {
+    // @codingStandardsIgnoreStart
+    // Stream wrappers are instantiated by PHP's stream system, not Drupal's DI container.
+    $this->remoteInstance = \Drupal::config('iq_stage_file_proxy.settings')->get('remote_instance');
+    $this->offload = \Drupal::config('iq_stage_file_proxy.settings')->get('offload') ?: $this->offload;
+    $this->fileSystem = \Drupal::service('file_system');
+    // @codingStandardsIgnoreEnd
   }
 
   /**
@@ -144,10 +128,9 @@ class LocalDevPublicStream extends PublicStream {
   private function offloadRemoteAsset($remotePath, $localPath) {
     // Get the data.
     $data = \file_get_contents($remotePath);
-    // Save the file locally as the original request path.
-    $dirs = dirname(DRUPAL_ROOT . '/' . $localPath);
-    is_dir($dirs) || $this->fileSystem->mkdir($dirs, NULL, TRUE);
-    \file_put_contents(DRUPAL_ROOT . '/' . $localPath, $data);
+    $directory = dirname($localPath);
+    $this->fileSystem->prepareDirectory($directory, $this->fileSystem::CREATE_DIRECTORY | $this->fileSystem::MODIFY_PERMISSIONS);
+    \file_put_contents($localPath, $data);
   }
 
 }
